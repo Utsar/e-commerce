1f0fa37b6ef3f5d4af655deee2371a2e170b2886
Kristian Utsar <utsaru@gmail.com>
2022-02-07 23:03:30 +0000

    finishes custom slider effect

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,12 +1,14 @@
 import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
+import { useState } from "react";
 import styled from "styled-components";
-import beer from "../Assets/Beer1.jpg";
+import { sliderItems } from "../data";
 
 const Container = styled.div`
   width: 100%;
   height: 100vh;
   display: flex;
   position: relative;
+  overflow: hidden;
 `;
 const Arrow = styled.div`
   width: 50px;
@@ -24,10 +26,14 @@ const Arrow = styled.div`
   margin: auto;
   cursor: pointer;
   opacity: 0.5;
+  z-index: 2;
 `;
 
 const Wrapper = styled.div`
   height: 100%;
+  display: flex;
+  transition: all 1.5s ease;
+  transform: translateX(${(props) => props.slideIndex * -100}vw);
 `;
 
 const Slide = styled.div`
@@ -35,6 +41,7 @@ const Slide = styled.div`
   height: 100vh;
   display: flex;
   align-items: center;
+  background-color: #${(props) => props.bg};
 `;
 const ImageContainer = styled.div`
   flex: 1;
@@ -45,23 +52,55 @@ const Image = styled.img`
 `;
 const InfoContainer = styled.div`
   flex: 1;
+  padding: 50px;
+`;
+
+const Title = styled.h1`
+  font-size: 70px;
+`;
+const Description = styled.p`
+  margin: 50px 0;
+  font-size: 20px;
+  font-weight: 500;
+  letter-spacing: 2.5px;
+`;
+const Button = styled.button`
+  padding: 10px;
+  font-size: 20px;
+  background-color: transparent;
+  cursor: pointer;
 `;
 
 const Slider = () => {
+  // handling right and left arrow clicks for sliders
+  const [slideIndex, setSlideIndex] = useState(0);
+  const handleClick = (direction) => {
+    if (direction === "left") {
+      setSlideIndex(slideIndex > 0 ? slideIndex - 1 : 2);
+    } else {
+      setSlideIndex(slideIndex < 2 ? slideIndex + 1 : 0);
+    }
+  };
   return (
     <Container>
-      <Arrow direction="left">
+      <Arrow direction="left" onClick={() => handleClick("left")}>
         <ArrowLeftOutlined />
       </Arrow>
-      <Wrapper>
-        <Slide>
-          <ImageContainer>
-            <Image src={beer} />
-          </ImageContainer>
-          <InfoContainer></InfoContainer>
-        </Slide>
+      <Wrapper slideIndex={slideIndex}>
+        {sliderItems.map((item) => (
+          <Slide key={item.id} bg={item.bg}>
+            <ImageContainer>
+              <Image src={item.image} />
+            </ImageContainer>
+            <InfoContainer>
+              <Title>{item.title}</Title>
+              <Description>{item.description}</Description>
+              <Button>Shop now</Button>
+            </InfoContainer>
+          </Slide>
+        ))}
       </Wrapper>
-      <Arrow direction="right">
+      <Arrow direction="right" onClick={() => handleClick("right")}>
         <ArrowRightOutlined />
       </Arrow>
     </Container>
---
+++ src/data.js
@@ -0,0 +1,26 @@
+import beer1 from "./Assets/Beer1.jpg";
+import beer2 from "./Assets/Beer2.jpg";
+import cocktail from "./Assets/cocktail1.jpg";
+export const sliderItems = [
+  {
+    id: 1,
+    image: { beer1 },
+    title: "FEEL THE DIFFERENCE",
+    description: "DON'T COMPROMISE ON TASTE! GET THE JUGS NOW.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    image: { beer2 },
+    title: "THE COLLECTION",
+    description: "SEASONED BEERS FOR THE ADVENTURE! ENTER NOW.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    image: { cocktail },
+    title: "MIX IT UP",
+    description: "COCKTAILS FOR EVERYONE! SHAKE OR STIR.",
+    bg: "fbf0f4",
+  },
+];
