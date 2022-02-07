002e190be6544a84c49f9e3a08829ed2c792e4a1
Kristian Utsar <utsaru@gmail.com>
2022-02-07 21:01:20 +0000

    adds slider boiler and announcement components

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 30px;
+  background-color: teal;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  font-weight: 500;
+`;
+
+const Announcement = () => {
+  return (
+    <Container>
+      Hire Kristian Now! Free round of beers on first Friday!
+    </Container>
+  );
+};
+
+export default Announcement;
---
+++ src/components/Slider.jsx
@@ -0,0 +1,71 @@
+import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
+import styled from "styled-components";
+import beer from "../Assets/Beer1.jpg";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  position: relative;
+`;
+const Arrow = styled.div`
+  width: 50px;
+  height: 50px;
+  background-color: whitesmoke;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: ${(props) => props.direction === "left" && "10px"};
+  right: ${(props) => props.direction === "right" && "10px"};
+  margin: auto;
+  cursor: pointer;
+  opacity: 0.5;
+`;
+
+const Wrapper = styled.div`
+  height: 100%;
+`;
+
+const Slide = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+`;
+const ImageContainer = styled.div`
+  flex: 1;
+  height: 100%;
+`;
+const Image = styled.img`
+  height: 80%;
+`;
+const InfoContainer = styled.div`
+  flex: 1;
+`;
+
+const Slider = () => {
+  return (
+    <Container>
+      <Arrow direction="left">
+        <ArrowLeftOutlined />
+      </Arrow>
+      <Wrapper>
+        <Slide>
+          <ImageContainer>
+            <Image src={beer} />
+          </ImageContainer>
+          <InfoContainer></InfoContainer>
+        </Slide>
+      </Wrapper>
+      <Arrow direction="right">
+        <ArrowRightOutlined />
+      </Arrow>
+    </Container>
+  );
+};
+
+export default Slider;
