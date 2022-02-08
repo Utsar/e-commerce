e622d3b0aac6817831d421131e9f6ea4ab3a7f32
Kristian Utsar <utsaru@gmail.com>
2022-02-08 10:55:06 +0000

    adds categories and dummy data

--- src/data.js
+++ src/data.js
@@ -1,26 +1,93 @@
-import beer1 from "./Assets/Beer1.jpg";
-import beer2 from "./Assets/Beer2.jpg";
-import cocktail from "./Assets/cocktail1.jpg";
+// import beer1 from "./Assets/Beer1.jpg";
+// import beer2 from "./Assets/Beer2.jpg";
+// import cocktail from "./Assets/cocktail1.jpg";
 export const sliderItems = [
   {
     id: 1,
-    image: { beer1 },
+    image:
+      "https://images.pexels.com/photos/1672304/pexels-photo-1672304.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     title: "FEEL THE DIFFERENCE",
     description: "DON'T COMPROMISE ON TASTE! GET THE JUGS NOW.",
     bg: "f5fafd",
   },
   {
     id: 2,
-    image: { beer2 },
+    image:
+      "https://www.italianfoodexcellence.com/wp-content/uploads/sites/27/2017/06/Ichnusa_Bicchiere.jpg",
     title: "THE COLLECTION",
     description: "SEASONED BEERS FOR THE ADVENTURE! ENTER NOW.",
     bg: "fcf1ed",
   },
   {
     id: 3,
-    image: { cocktail },
+    image:
+      "https://images.pexels.com/photos/5947019/pexels-photo-5947019.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     title: "MIX IT UP",
     description: "COCKTAILS FOR EVERYONE! SHAKE OR STIR.",
     bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    image:
+      "https://images.pexels.com/photos/1267700/pexels-photo-1267700.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    title: "ASSORTED CRAFT!",
+  },
+  {
+    id: 2,
+    image:
+      "https://images.pexels.com/photos/1089932/pexels-photo-1089932.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    title: "BOTTLED UP!",
+  },
+  {
+    id: 3,
+    image:
+      "https://images.pexels.com/photos/1283219/pexels-photo-1283219.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    title: "SPIRITUAL!",
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    image:
+      "https://images.pexels.com/photos/2286972/pexels-photo-2286972.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  },
+  {
+    id: 2,
+    image:
+      "https://images.pexels.com/photos/1435598/pexels-photo-1435598.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  },
+  {
+    id: 3,
+    image:
+      "https://images.pexels.com/photos/2565587/pexels-photo-2565587.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  },
+  {
+    id: 4,
+    image:
+      "https://images.pexels.com/photos/4071548/pexels-photo-4071548.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  },
+  {
+    id: 5,
+    image:
+      "https://images.pexels.com/photos/4876531/pexels-photo-4876531.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  },
+  {
+    id: 6,
+    image:
+      "https://images.pexels.com/photos/1280364/pexels-photo-1280364.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  },
+  {
+    id: 7,
+    image:
+      "https://images.pexels.com/photos/9009821/pexels-photo-9009821.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  },
+  {
+    id: 8,
+    image:
+      "https://images.pexels.com/photos/7809787/pexels-photo-7809787.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260",
   },
 ];
