a60c01fe449ac6f2a80520a32342b31a365dbc5b
Kristian Utsar <utsaru@gmail.com>
2022-02-11 16:49:00 +0000

    adds jsnonwebtoken

--- server/routes/auth/auth.js
+++ server/routes/auth/auth.js
@@ -1,6 +1,7 @@
 import express from "express";
 import User from "../users/userSchema.js";
 import CryptoJS from "crypto-js";
+import jwt from "jsonwebtoken";
 
 const authRouter = express.Router();
 // register
@@ -8,6 +9,7 @@ authRouter.post("/register", async (req, res, next) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
+    // encrypting password
     password: CryptoJS.AES.encrypt(
       req.body.password,
       process.env.SECRET_PASSWORD
@@ -27,14 +29,27 @@ authRouter.post("/login", async (req, res, next) => {
     const user = await User.findOne({
       username: req.body.username,
     });
+    // checking if user exists
     !user && res.status(401).send({ message: "User not found" });
+
+    // checking if password is correct
     const decryptedPassword = CryptoJS.AES.decrypt(
       user.password,
       process.env.SECRET_PASSWORD
     ).toString(CryptoJS.enc.Utf8);
+    // verifying password
     decryptedPassword !== req.body.password &&
       res.status(401).send({ message: "Wrong password" });
-    res.status(200).send(user);
+    //   generating token
+    const token = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+    res.status(200).send({ user, token });
   } catch {
     next(error);
   }
