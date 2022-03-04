e60cfb6a99ce4cfab17d81231a54c41f71865a7f
Kristian Utsar <utsaru@gmail.com>
2022-03-04 22:27:41 +0000

    bug fixes, routes for pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,25 @@
 import "./App.css";
-// import Cart from "./pages/Cart";
-// import Login from "./pages/Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
 import Home from "./pages/Home";
-// import ProductList from "./pages/ProductList";
-// import ProductSingle from "./pages/ProductSingle";
-// import Register from "./pages/Register";
+import ProductList from "./pages/ProductList";
+import ProductSingle from "./pages/ProductSingle";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <>
-      <Home />
-      {/* <ProductList /> */}
-      {/* <ProductSingle /> */}
-      {/* <Register /> */}
-      {/* <Login /> */}
-      {/* <Cart /> */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<ProductList />} />
+          <Route path="/products/:id" element={<ProductSingle />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </Router>
     </>
   );
 }
--- server/routes/product/product.js
+++ server/routes/product/product.js
@@ -7,7 +7,7 @@ const productRouter = express.Router();
 // Create
 
 productRouter.post("/", verifyToken, async (req, res) => {
-  const newProduct = new Product(req.bodd);
+  const newProduct = new Product(req.body);
   try {
     const savedProduct = await newProduct.save();
     res.status(200).send(savedProduct);
